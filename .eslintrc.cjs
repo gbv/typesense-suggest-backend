ab68a537355565eb5a7d559d87f52036b5a01ee8
Stefan Peters <Stefan.Peters@gbv.de>
2022-12-06 10:27:25 +0100

    Set up repository and install some dependencies

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: [
+    "gbv",
+  ],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+}
