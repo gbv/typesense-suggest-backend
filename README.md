4cec57a0a1fd06318ab7b2284a355d2458bee4dc
Stefan Peters <Stefan.Peters@gbv.de>
2022-12-09 11:24:53 +0100

    Add first implementation of backend server
    Also updates README with more detailed info about requirements and setup.

--- README.md
+++ README.md
@@ -3,6 +3,12 @@ Experimental backend for mapping suggestions using Typesense, to be used in Coco
 
 Currently not yet usable.
 
+## Requirements
+
+- Node.js 14 or later
+- A running [Typesense](https://typesense.org) server
+  - Access details need to be configured in `config/config.json`
+
 ## Setup
 
 ```bash
@@ -11,9 +17,16 @@ npm ci
 # Copy and adjust default config
 cp config/config.default.json config/config.json
 editor config/config.json
-# Run script
-node index.js
+# Run setup (currently defaults to Basisklassifikation as no other target is supported)
+# Note: The first run will take a long time as a lot of data will have to be loaded from APIs. However, most data is cached, so subsequent runs will be much faster.
+npm run setup
+# Start the server (default port is 3021)
+npm run start
 ```
+
+## Usage
+
+The service offers a [jskos-server](https://github.com/gbv/jskos-server) compatible `/search` endpoint. Currently supported query parameters: `voc` (vocabulary URI, required), `search` (search string, required), `limit` (optional), `offset` (optional)
 
 ## To-Dos
 
---
+++ server.js
@@ -0,0 +1,83 @@
+import config from "./config/index.js"
+import * as utils from "./lib/utils.js"
+
+// Initialize express with settings
+import express from "express"
+export const app = express()
+app.set("json spaces", 2)
+if (config.proxies && config.proxies.length) {
+  app.set("trust proxy", config.proxies)
+}
+
+app.use((req, res, next) => {
+  if (req.headers.origin) {
+    // Allow all origins by returning the request origin in the header
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin)
+  } else {
+    // Fallback to * if there is no origin in header
+    res.setHeader("Access-Control-Allow-Origin", "*")
+  }
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+  res.setHeader("Access-Control-Allow-Methods", "GET")
+  res.setHeader("Access-Control-Expose-Headers", "X-Total-Count, Link")
+  res.setHeader("Content-Type", "application/json; charset=utf-8")
+  next()
+})
+
+// Load compatible schemes from database
+import Database from "better-sqlite3"
+const db = new Database(config.database)
+const schemes = db.prepare("SELECT * FROM schemes").all().map(s => Object.assign({ _key: s.key }, JSON.parse(s.json)))
+
+import typesense from "./lib/typesense.js"
+import jskos from "jskos-tools"
+
+app.get(
+  "/search",
+  utils.wrappers.async(async (req) => {
+    // Check if vocabulary is compatbile
+    const scheme = schemes.find(s => jskos.compare(s, { uri: req.query.voc }))
+    if (!scheme) {
+      throw new Error(`Incompatible scheme: ${req.query.voc}`)
+    }
+    if (!req.query.search) {
+      return []
+    }
+    // Get results from Typesense backend
+    const collection = `${scheme._key}-suggestions`
+    const limit = parseInt(req.query.limit) || 100
+    const offset = parseInt(req.query.offset) || 0
+    const per_page = limit
+    const page = Math.floor(offset / limit + 1)
+    const result = await typesense.search(collection, req.query.search, ["identifier", "prefLabel", "altLabel", "mappingLabelsExactClose", "mappingLabelsNarrowBroad", "notes"], { per_page, page })
+    // Return concept data only
+    return result.hits.map(hit => {
+      const concept = hit.document.concept
+      // Reduce concept data a bit
+      // TODO: Maybe do this during import already
+      delete concept._mappings
+      delete concept.ancestors
+      delete concept.narrower
+      ;["broader", "inScheme"].forEach(key => {
+        if (Array.isArray(concept[key])) {
+          concept[key] = concept[key].map(({ uri }) => ({ uri }))
+        }
+      })
+      return concept
+    })
+  }),
+  utils.returnJSON,
+)
+
+const start = async () => {
+  // if (config.env == "test") {
+  //   const portfinder = require("portfinder")
+  //   portfinder.basePort = config.port
+  //   config.port = await portfinder.getPortPromise()
+  // }
+  app.listen(config.port, () => {
+    config.log(`Now listening on port ${config.port}`)
+  })
+}
+// Start express server immediately even if database is not yet connected
+start()
