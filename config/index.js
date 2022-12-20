07bf571a5d4f11b43050df6ed12168c069c0d765
Stefan Peters <Stefan.Peters@gbv.de>
2022-12-20 11:42:41 +0100

    Allow config file override via CONFIG_FILE environment variable
    Relative to the `config/` directory.

--- config/index.js
+++ config/index.js
@@ -1,7 +1,17 @@
 // Read config.json file. In the future, this could be replaced with an import assertion.
 import { readFileSync } from "fs"
 const configDefault = JSON.parse(readFileSync(new URL("./config.default.json", import.meta.url)))
-const config = Object.assign({}, configDefault, JSON.parse(readFileSync(new URL("./config.json", import.meta.url))))
+
+// Allow override of config file path via CONFIG_FILE
+const configFile = process.env.CONFIG_FILE ?? "./config.json"
+let config
+
+try {
+  config = Object.assign({}, configDefault, JSON.parse(readFileSync(new URL(configFile, import.meta.url))))
+} catch (error) {
+  console.error(new Date(), `Error loading config file at ${configFile}, exiting...`)
+  process.exit(1)
+}
 
 if (![true, false, "log", "warn", "error"].includes(config.verbosity)) {
   console.warn(`Invalid verbosity value "${config.verbosity}", defaulting to "${configDefault.verbosity}" instead.`)
@@ -24,5 +34,7 @@ config.error = (...args) => {
     console.error(new Date(), ...args)
   }
 }
+
+config.log(`Configuration loaded from ${configFile}.`)
 
 export default config
